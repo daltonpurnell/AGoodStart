56bc3631d71b47e4f75e673866d85600eb25f5a8
User <desiree@MacBook-Pro.home>
2015-02-06 10:59:28 -0700

    Initial Commit

---
+++ pre-course-git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pre-course-git
+//
+//  Created by User on 2/6/15.
+//  Copyright (c) 2015 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pre-course-git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pre-course-git
+//
+//  Created by User on 2/6/15.
+//  Copyright (c) 2015 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
